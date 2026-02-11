c9c66c402d226cc42dc59a89c9a1f1ee266b0740
Aravind Ganipisetty <aravindganipisetty@gmail.com>
2026-02-11 16:08:11 -0500

    Add files via upload

---
+++ landmarks/LandmarkList.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+struct LandmarkList: View {
+    @Environment(ModelData.self) var modelData
+    @State private var showFavoritesOnly = false
+
+    var filteredLandmarks: [Landmark] {
+        modelData.landmarks.filter { landmark in
+            (!showFavoritesOnly || landmark.isFavorite)
+        }
+    }
+
+    var body: some View {
+        NavigationStack {
+            List {
+                Toggle("Favorites only", isOn: $showFavoritesOnly)
+
+                ForEach(filteredLandmarks) { landmark in
+                    NavigationLink {
+                        LandmarkDetail(landmark: landmark)
+                    } label: {
+                        LandmarkRow(landmark: landmark)
+                    }
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+#Preview {
+    LandmarkList()
+        .environment(ModelData())
+}
